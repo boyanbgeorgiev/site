acbfaecc7a0cef2ca5261b4ba83e1140a4c2817c
boyanbgeorgiev <boyan.b.georgiev@gmail.com>
2024-04-16 20:44:46 +0300

    Update connection.php

--- login/connection.php
+++ login/connection.php
@@ -5,7 +5,7 @@ $dbuser = "root";
 $dbpass = "bobiubuntu";
 $dbname = "login_sample_DB";
 
-if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass))
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
 {
     die("fail");
 }
