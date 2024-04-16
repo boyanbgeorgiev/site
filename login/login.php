47ad79e50c4c782663755a8997d81f7b722308c3
boyanbgeorgiev <boyan.b.georgiev@gmail.com>
2024-04-16 19:27:46 +0300

    made login

---
+++ login/login.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <div id="box">
+
+       <form method="post">
+            <input type="text" name="user_name"><br><br>
+            <input type="password" name="password"><br><br>
+
+            <input type="submit" value="Login"><br><br>
+
+            <a href="signup.php">Signup</a>
+       </form> 
+
+    </div>
+    
+</body>
+</html>
