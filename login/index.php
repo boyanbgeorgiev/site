a888dc05ac486efb43dc783c6d54b8fad4f6ae47
boyanbgeorgiev <boyan.b.georgiev@gmail.com>
2024-04-16 18:40:56 +0300

    made index page

---
+++ login/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My website</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>This is the index page</p>
+
+    <br>
+    Hello, Username!
+</body>
+</html>
