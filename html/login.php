1116e8fb81a67f3ed0b31be5607b64ad2bc86a09
boyanbgeorgiev <boyan.b.georgiev@gmail.com>
2024-04-27 01:16:59 +0300

    cookies

--- html/login.php
+++ html/login.php
@@ -11,9 +11,34 @@ if(isset($_SESSION['user_name'])) {
     exit; // Terminate script execution
 }
 
+// Check if the remember me cookie exists and contains valid data
+if(isset($_COOKIE['rememberme'])) {
+    list($user_id, $token) = explode(':', $_COOKIE['rememberme']);
+
+    // Fetch the user from the database based on the user ID
+    $query = "SELECT * FROM users WHERE user_id = '$user_id' LIMIT 1";
+    $result = mysqli_query($con, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $user_data = mysqli_fetch_assoc($result);
+
+        // Verify the token stored in the database with the token from the cookie
+        if (hash_equals($user_data['remember_token'], hash('sha256', $token))) {
+            // Set user session variables
+            $_SESSION['user_id'] = $user_data['user_id'];
+            $_SESSION['user_name'] = $user_data['user_name'];
+
+            // Redirect to the welcome page
+            header("Location: welcome.php");
+            exit; // Terminate script execution
+        }
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $user_name = $_POST['user_name'];
     $password = $_POST['password'];
+    $remember_me = isset($_POST['remember_me']); // Check if remember me checkbox is checked
 
     if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
         // Read from database
@@ -32,6 +57,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 // Set user session variables
                 $_SESSION['user_id'] = $user_data['user_id'];
                 $_SESSION['user_name'] = $user_data['user_name'];
+
+                // Set remember me cookie if checked
+                if ($remember_me) {
+                    $token = bin2hex(random_bytes(16)); // Generate a random token
+                    $expiry_time = time() + (30 * 24 * 3600); // 30 days expiration time
+                    $token_hash = hash('sha256', $token);
+                    setcookie('rememberme', $user_data['user_id'] . ':' . $token, $expiry_time, '/');
+                    
+                    // Update the remember token in the database
+                    $update_query = "UPDATE users SET remember_token = '$token_hash' WHERE user_id = '{$user_data['user_id']}'";
+                    mysqli_query($con, $update_query);
+                }
 
                 // Redirect to the welcome page
                 header("Location: welcome.php");
@@ -82,7 +119,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         </form> 
     </div>
 <div class="links">
-    <a href="signup.php">CREATE ACCOUNT</a>
+    <a href="signup.php">CREATE ACCOUNT</a><br><br>
     <a href="logout.php">LOG OUT</a>
 </div>
 </body>
